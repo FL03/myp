fd8a00eebf45d5284487c4500434f883a6b51df2
FL03 <jo3mccain@icloud.com>
2022-06-01 13:19:03 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -14,19 +14,18 @@ async def load_current_user(current_user: User = Depends(get_current_active_user
     return current_user
 
 
-@router.post("/{username}")
+@router.post("/")
 async def create_user(username: str, password: str):
     return db.put(UserIn(username=username, hashed_password=get_password_hash(password)).dict())
 
 
-@router.put("/{current_user.username}", dependencies=[Depends(get_current_active_user)], response_model=UserIn)
+@router.put("/", dependencies=[Depends(get_current_active_user)], response_model=UserIn)
 async def update_current_user(current_user: UserIn = Depends(get_current_active_user), updates: UserIn = None):
-    user = current_user
-    db.update(key=user.key, updates=updates.dict(exclude_unset=True))
-    return UserIn(**db.get(user.key))
+    db.update(key=current_user.key, updates=dict(current_user, **updates.dict(exclude_unset=True)))
+    return UserIn(**db.get(current_user.key))
 
 
-@router.delete("/user/{key}", response_model=Status)
+@router.delete("/", response_model=Status)
 async def delete_user(key: str):
     for i in db.fetch().items:
         if key == i["key"]:
